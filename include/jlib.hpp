2a9e5044b03300b292fdebd5b54c856312f12da2
Fábio Mestre <fabio.mestre@codeplay.com>
2022-05-25 18:29:02 +0100

    Add sub

--- include/jlib.hpp
+++ include/jlib.hpp
@@ -3,4 +3,6 @@
 namespace jlib {
 int add2(int a, int b);
 int add3(int a, int b, int c);
+int sub2(int a, int b);
+int sub3(int a, int b, int c);
 }  // namespace jlib
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 namespace jlib {
 int add2(int a, int b) { return a + b; }
 int add3(int a, int b, int c) { return a + b + c; }
+int sub2(int a, int b) { return a - b; }
+int sub3(int a, int b, int c) { return a - b - c; }
 }  // namespace jlib
