e422d4fe607cf79330d858f5ce766b48d9febc9d
Fábio Mestre <fabio.mestre@codeplay.com>
2022-05-26 12:01:20 +0100

    Added multiplication

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,6 @@ int add2(int a, int b) { return a + b; }
 int add3(int a, int b, int c) { return a + b + c; }
 int sub2(int a, int b) { return a - b; }
 int sub3(int a, int b, int c) { return a - b - c; }
+int mul2(int a, int b) { return a * b; }
+int mul3(int f, int a, int b) { return f * a * b; }
 }  // namespace jlib
